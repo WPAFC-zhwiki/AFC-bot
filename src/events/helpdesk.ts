d5911fc7fd9eb3b61434165d42f2a1915c19fead
LuciferianThomas <officialtccgplayz@gmail.com>
2021-08-05 13:49:34 +0800

    123

--- src/events/helpdesk.ts
+++ src/events/helpdesk.ts
@@ -25,7 +25,7 @@ const Event: event = {
       return (
         data.wiki === 'zhwiki'
         && data.title === 'WikiProject:建立條目/詢問桌'
-        && data.length.old >= data.length.new + 10
+        && data.length && data.length.old >= data.length.new + 10
       )
     }, async (data) => {
       let { compare } = await mwBot.request({
